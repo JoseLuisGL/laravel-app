0de796ff4e25fee436ef3b1f0923e73c39bb1b84
JoseLuisGL <jgomez_21@alu.uabcs.mx>
2024-11-19 11:14:44 -0700

    Creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,42 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+//Vista
+Route::get('/visitante', function () {
+    return view('visitante');
+});
+
+//Operaciones
+Route::get('/operacion/{tipo}/{n1}/{n2}', function ($tipo,$n1,$n2) {
+
+    switch ($tipo) {
+        case 'suma':
+            $resultado = $n1 + $n2;
+            break;
+        case 'resta':
+            $resultado = $n1 - $n2;
+            break;   
+        case 'multiplicacion':
+            $resultado = $n1 * $n2;
+            break;
+        case 'division':
+            $resultado = $n1 /  $n2;
+            break;
+
+        default:
+            break;
+    }
+
+    return 'Resultado: '. $resultado;
+})->where(['n1' => '[0-9]+', 'n2' => '[0-9]+']);
+
+//Saludo
+Route::get('/saludar/{name}/{lastname?}', function ($name,$lastname) {
+
+    return 'Hola '. $name . $lastname;
+})->where('name', '[A-Za-z]+');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
